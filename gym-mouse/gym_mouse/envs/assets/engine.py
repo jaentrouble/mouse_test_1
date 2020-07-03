b8d59f46e13a35373f12b1037808de46ea8e7ca8
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-07-04 00:07:19 +0900

    Added managers.py
    TODO: implement collision manager

--- gym-mouse/gym_mouse/envs/assets/engine.py
+++ gym-mouse/gym_mouse/envs/assets/engine.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .managers import *
 
 #TODO: Implement simple things to the engine and make the gym run first.
 #       If it runs well, then add all fancy stuffs.
@@ -27,29 +28,3 @@ class Engine():
         return self._image.copy()
 
     
-class ThingsManager():
-    """
-    Manages all the things in Engine.
-    """
-    def __init__(self):
-        self._new_id = 0
-        self._id_dic = {}
-
-    def regist(self, thing):
-        """Register a new thing to the manager and return the id of that"""
-        thing_id = self._new_id
-        self._id_dic[thing_id] = thing
-        self._new_id += 1
-        return thing_id
-
-    def id_(self, ID) :
-        """Return the object of the ID"""
-        return self._id_dic[ID]
-
-    def indices(self, ID) :
-        """Return the indices of the thing of the ID"""
-        return self._id_dic[ID].indices
-
-    def color(self, ID) :
-        """Return the color of the thing of the ID"""
-        return self._id_dic[ID].color
